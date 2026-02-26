b997846ae5858a56c6b974ada2b7ba1fe5e11bb9
Hardik Boricha <hardik.coderkube@gmail.com>
2026-02-26 10:06:12 +0530

    b14 data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <h1>data1</h1>
-    <h1>data2</h1>
-    <h1>b11</h1>
-    <h1>b13</h1>
-    <h1>b1</h1>
-
+      <h1>data1</h1>
+      <h1>data2</h1>
+      <h1>b11</h1>
+      <h1>b13</h1>
+      <h1>b1</h1>
+      <h1>b14</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
